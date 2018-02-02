45432e9ad91aaa409e148a8dbdc8e7bd4c096c71
blowder <blowder@mail.ru>
2018-02-02 16:05:42 +0200

    Multithreading files analyze was added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
 import org.kohsuke.args4j.CmdLineException;
-import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
 
 import javax.xml.XMLConstants;
 import javax.xml.transform.stream.StreamSource;
@@ -9,57 +7,51 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class Main {
-
-
-    public static void main(String[] args) throws CmdLineException, IOException, SAXException {
-        RunOptions runOptions = new RunOptions().parseArgs(args);
-
-        Schema schema = initSchema(runOptions.getXsdPath());
-
-        //TODO: parallel and add correct handlers
-        Validator validator = schema.newValidator();
-        validator.setErrorHandler(
-                new ErrorHandler() {
-                    @Override
-                    public void warning(SAXParseException e) throws SAXException {
-
-                    }
-
-                    @Override
-                    public void error(SAXParseException e) throws SAXException {
-                        //todo: save errors to log file
-                        System.out.println(String.format("Error happened in line:%s with message:%s", e.getLineNumber(), e.getMessage()));
-                    }
-
-                    @Override
-                    public void fatalError(SAXParseException e) throws SAXException {
-
-                    }
-                }
-        );
-        DirectoryStream<Path> paths = Files.newDirectoryStream(runOptions.getXmlsDir().toPath());
-        for (Path path : paths) {
-            File source = path.toFile();
-            if (source.getName().toLowerCase().endsWith(".xml")) {
-                validator.validate(new StreamSource(source));
-                System.out.println(source.getAbsolutePath());
-            } else {
-                System.out.println("Not a xml, skipped: " + source.getAbsolutePath());
-            }
-
-        }
-
-    }
 
     private static Schema initSchema(File xsdFile) throws SAXException {
         SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         return factory.newSchema(new StreamSource(xsdFile));
     }
+
+    public static void main(String[] args) throws CmdLineException, IOException, SAXException {
+        RunOptions runOptions = new RunOptions().parseArgs(args);
+
+        Schema schema = initSchema(runOptions.getXsdPath());
+        FileWriter logWriter = new FileWriter(runOptions.getLogFile(), true);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+
+        DirectoryStream<Path> paths = Files.newDirectoryStream(runOptions.getXmlsDir().toPath());
+        for (Path path : paths) {
+            executorService.submit(() -> {
+                File source = path.toFile();
+                if (source.getName().toLowerCase().endsWith(".xml")) {
+                    Validator validator = schema.newValidator();
+                    validator.setErrorHandler(new XmlErrorsCollector(source.getName(), logWriter));
+                    try {
+                        validator.validate(new StreamSource(source));
+                        logWriter.flush();
+                    } catch (SAXException | IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    System.out.println("Not a xml, skipped: " + source.getAbsolutePath());
+                }
+            });
+        }
+        executorService.shutdown();
+        System.out.println("---All jobs were scheduled---");
+    }
+
+
 }
--- src/main/java/XmlErrorsCollector.java
+++ src/main/java/XmlErrorsCollector.java
@@ -2,28 +2,41 @@ import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class XmlErrorsCollector implements ErrorHandler {
-    private List<String> validationLog = new ArrayList<>();
+    private static final String LOG_MESSAGE_PATTERN = "[%1$-4s][%2$-80s][line:%3$-4s,col:%4$-4s][%5$s]\n";
+    private final FileWriter log;
+    private final String fileName;
+
+
+    public XmlErrorsCollector(String fileName, FileWriter log) {
+        this.fileName = fileName;
+        this.log = log;
+    }
 
     @Override
     public void warning(SAXParseException exception) throws SAXException {
-
+        log(String.format(LOG_MESSAGE_PATTERN, "WARN", fileName, exception.getLineNumber(), exception.getColumnNumber(), exception.getMessage()));
     }
 
     @Override
     public void error(SAXParseException exception) throws SAXException {
-
+        log(String.format(LOG_MESSAGE_PATTERN, "ERROR", fileName, exception.getLineNumber(), exception.getColumnNumber(), exception.getMessage()));
     }
 
     @Override
     public void fatalError(SAXParseException exception) throws SAXException {
-
+        log(String.format(LOG_MESSAGE_PATTERN, "FATAL", fileName, exception.getLineNumber(), exception.getColumnNumber(), exception.getMessage()));
     }
 
-    public List<String> getValidationLog() {
-        return validationLog;
+    private void log(String message) throws SAXException {
+        try {
+            log.append(message);
+        } catch (IOException e) {
+            throw new SAXException(e);
+        }
     }
+
 }
